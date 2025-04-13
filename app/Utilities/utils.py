9e4bb150b7ae22c6628c12686946256ab10882ad
nivesh1000 <nivesh.nk1001@gmail.com>
2025-04-13 08:43:02 +0530

    a

--- app/Utilities/utils.py
+++ app/Utilities/utils.py
@@ -67,7 +67,7 @@ def generate_today_email_url() -> str:
     # Construct the URL for filtering emails by receivedDateTime
     url = (
         "https://graph.microsoft.com/v1.0/me/messages?"
-        "$top=100&"
+        "$top=20&"
         "$select=toRecipients,from,subject,body,receivedDateTime,internetMessageHeaders&"
         f"$filter=receivedDateTime ge {start_time} and receivedDateTime le {end_time}"
         "&$orderby=receivedDateTime DESC"
